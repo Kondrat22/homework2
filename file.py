b501b630cedd89ac4cc50dbda98548a904ff5760
Kondrat22 <kondrat_va@bk.ru>
2024-07-21 13:20:58 +0300

    Add files via upload

---
+++ file.py
@@ -0,0 +1,5 @@
+summ_homeworks = "12" 
+time_homeworks = "1.5"
+name = "Python"
+time_for_one = 1.5/12
+print ( 'Курс:' , name , ', всего выполнено заданий' , summ_homeworks, ', затрачено часов' , time_homeworks , ', среднее время выполнения' , time_for_one , 'часа')
