dd458f2ced861493298371db018e47e9fa5e1a57
Kondrat22 <kondrat_va@bk.ru>
2024-07-22 13:40:53 +0300

    Update file.py

--- file.py
+++ file.py
@@ -1,5 +1,5 @@
 summ_homeworks = "12" 
 time_homeworks = "1.5"
 name = "Python"
-time_for_one = 1.5/12
+time_for_one = time_homeworks / summ_homeworks
 print ( 'Курс:' , name , ', всего выполнено заданий' , summ_homeworks, ', затрачено часов' , time_homeworks , ', среднее время выполнения' , time_for_one , 'часа')
